f5b87abe481f16ee823952a630cb32629f55b1bc
DAQEM <daqemyt@gmail.com>
2023-10-02 15:01:00 +0200

    Added Migrations

---
+++ Services/DeviceManagerService/Migrations/20231002125855_AddedOnDeleteCascade.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceManagerService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedOnDeleteCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
