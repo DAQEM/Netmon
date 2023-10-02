0f972458b8e96218c0c519d2168d08f9df46432c
DAQEM <daqemyt@gmail.com>
2023-10-02 10:50:14 +0200

    Removed Models, Controllers and Migrations from DeviceManagerService.csproj

--- Services/DeviceManagerService/Models/DeviceOID/GetDeviceOIDModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DeviceManagerService.Models.DeviceOID;
-
-public class GetDeviceOIDModel
-{
-    public Guid Id { get; set; }
-    public Guid DeviceId { get; set; }
-    public DevicesLib.Entities.DeviceOIDDTO.DeviceOIDType Type { get; set; }
-    public string Value { get; set; }
-}
