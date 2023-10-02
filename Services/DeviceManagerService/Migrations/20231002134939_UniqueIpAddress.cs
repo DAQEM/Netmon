64f375ed689ac41f514eccfa0b2d84c8f0f03aa2
DAQEM <daqemyt@gmail.com>
2023-10-02 16:45:23 +0200

    Added migrations

---
+++ Services/DeviceManagerService/Migrations/20231002134939_UniqueIpAddress.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceManagerService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueIpAddress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Type",
+                table: "Interfaces",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AdminStatus",
+                table: "InterfaceMetrics",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OperationStatus",
+                table: "InterfaceMetrics",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "Devices",
+                type: "varchar(255)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PrivacyProtocol",
+                table: "DeviceConnections",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthProtocol",
+                table: "DeviceConnections",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_IpAddress",
+                table: "Devices",
+                column: "IpAddress",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_IpAddress",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "AdminStatus",
+                table: "InterfaceMetrics");
+
+            migrationBuilder.DropColumn(
+                name: "OperationStatus",
+                table: "InterfaceMetrics");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Interfaces",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "IpAddress",
+                table: "Devices",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrivacyProtocol",
+                table: "DeviceConnections",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthProtocol",
+                table: "DeviceConnections",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
