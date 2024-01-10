a3243dbfaedbb3e993af9ad81b58bbce9c2af899
DAQEM <daqemyt@gmail.com>
2024-01-10 16:59:48 +0100

    Added device add end-to-end tests

---
+++ Services/AdminWebApp/tests/device/add/add_device_e2e.ts
@@ -0,0 +1,26 @@
+import test, { expect } from '@playwright/test';
+
+test.describe.parallel('Add Device', () => {
+	const ipAddress = '1.1.1.1';
+
+	test('Add device to database', async ({ page }) => {
+		await page.goto('/device/add');
+
+		await page.selectOption('#version', '2');
+		await page.fill('#ip_address', ipAddress);
+		await page.fill('#port', '161');
+		await page.fill('#community', 'public');
+
+		await page.click('#submit');
+
+		const success = await page.innerText('#success-message');
+		expect(success).toBe('Device added successfully.');
+	});
+
+	test('Device should be added to table', async ({ page }) => {
+		await page.goto('/device');
+
+		const device = await page.innerText(`#${ipAddress} > td:nth-child(3)`);
+		expect(device).toBe(ipAddress);
+	});
+});
