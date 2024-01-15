e062fafecec405a391eb5e6011576ba9d3037639
DAQEM <daqemyt@gmail.com>
2024-01-15 20:49:13 +0100

    Fixed User Web App E2E Test

--- Services/UserWebApp/tests/test.ts
+++ Services/UserWebApp/tests/test.ts
@@ -1,6 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toHaveText('Welcome to Netmon!');
+test('log user in', async ({ page }) => {
+	await page.goto("/login")
+
+	await page.fill('input[name="username"]', 'demo');
+	await page.fill('input[name="password"]', 'P@ssw0rd!');
+
+	await page.click('button[type="submit"]');
+
+	expect(page.url()).toBe('http://localhost:4173/');
 });
